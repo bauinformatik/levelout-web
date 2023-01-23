f74c78887b50df8e0aa852600f59f09838240fcd
Amol Konde <amol.konde1@gmail.com>
2023-01-23 08:10:10 +0530

    first commit

---
+++ src/main/java/com/levelout/web/LeveloutWebApplication.java
@@ -0,0 +1,13 @@
+package com.levelout.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeveloutWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeveloutWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/levelout/web/LeveloutWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levelout.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeveloutWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
